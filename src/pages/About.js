31574523f758bd280984413042863b5668b16136
Pinkesh Choudhary <p.choudhary@contentbloom.com>
2023-09-16 11:58:08 +0530

    changes in about.js

--- src/pages/About.js
+++ src/pages/About.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export const About = () => {
   return (
-    <div>
+    <div className='about'>
       <a href='https://github.com/PinkeshChoudhary/github-explorer'>Pinkesh Choudhary</a>
       </div>
   )
